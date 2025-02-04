b1bffe4b7514f19ca607d54583d98b7e67bbfa3f
Marek Olszewski <marek@olszewski.software>
2025-02-04 18:32:35 +0100

    removed unsupported platforms directories

--- android/app/src/main/kotlin/com/example/morrowind_alchemy/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.morrowind_alchemy
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,20 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/user/Documents/sdk/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/user/Documents/dev/flutter/morrowind_alchemy" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/user/Documents/sdk/flutter"
+  "PROJECT_DIR=/home/user/Documents/dev/flutter/morrowind_alchemy"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/user/Documents/dev/flutter/morrowind_alchemy/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/user/Documents/dev/flutter/morrowind_alchemy/lib/main.dart"
+)
